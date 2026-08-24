24d3d5d226763f20e1dccf3c5611a597190512bd
andres2204 <medinaandres2204@gmail.com>
2026-08-24 01:12:44 -0500

    nice reto-4

--- guia-1/reto-4/reto-4.py
+++ guia-1/reto-4/reto-4.py
@@ -3,10 +3,27 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-images = ["camera", "cv2"]
-
+images = ["camera"]
 for img in images:
     cv2.namedWindow(img, cv2.WINDOW_NORMAL)
+
+colors = {
+    "blue": {
+        "low": np.array([100, 60, 40]),
+        "high": np.array([140, 255, 230]),
+        "color": (255,0,0)
+    },
+    "yellow": {
+        "low": np.array([20, 80, 80]),
+        "high": np.array([40, 255, 255]),
+        "color": (0, 255, 255)
+    },
+    "green": {
+        "low": np.array([40, 50, 50]),
+        "high": np.array([85, 255, 255]),
+        "color": (0, 255, 0)
+    }
+}
 
 loop = True
 while loop:
@@ -14,25 +31,28 @@ while loop:
 
     if not ret:
         break
-    
+   
     # Segmentation
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-   
-    low_blue = np.array([100, 60, 40])
-    high_blue = np.array([140, 255, 230])
-
-    blue_mask = cv2.inRange(hsv, low_blue, high_blue) 
     
-    contours, hierarchy = cv2.findContours(blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE);
-    sorted_contours_desc = sorted(contours, key=cv2.contourArea, reverse=True)
+    for key, c in colors.items():
+        mask = cv2.inRange(hsv, c["low"], c["high"])
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) == 0:
+            continue
 
-    cv2.drawContours(frame, sorted_contours_desc[0:4], -1, (0,0,255), 1, thickness=2)
+        contour = max(contours, key=cv2.contourArea)
+        if cv2.contourArea(contour) < 400:
+            continue
 
+        x, y, w, h = cv2.boundingRect(contour)
+
+        cv2.rectangle(frame, (x,y), (x+w, y+h), c["color"], 2)
+        cv2.putText(frame, key, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, c["color"], 2)
+    
 
     # show images
     cv2.imshow(images[0], frame)
-    cv2.imshow(images[1], blue_mask)
-    #cv2.imshow(images[1], threshold)
 
     # check views
     key = cv2.waitKey(10) & 0xFF
