215e33548b4be0a546d5090ec65026a8af0010dd
DavidAntolinez <antolinezchavarrodavid@gmail.com>
2026-09-14 21:18:32 -0500

    ajustes

--- guia-2/reto-4/reto-4.py
+++ guia-2/reto-4/reto-4.py
@@ -1,17 +1,15 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import warnings
+import matplotlib
+matplotlib.use('qtagg') # requerido en wayland junto con pyqt6
 warnings.filterwarnings('ignore')
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder, FunctionTransformer
-from sklearn.metrics import accuracy_score, precision_score, confusion_matrix, classification_report, ConfusionMatrixDisplay
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from pycaret.classification import setup,compare_models,tune_model,evaluate_model,predict_model,finalize_model,save_model,ensemble_model
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import ConfusionMatrixDisplay
+from pycaret.classification import setup,compare_models,tune_model,evaluate_model,predict_model,finalize_model,ensemble_model
 
 iris = load_iris()
 iris_features = pd.DataFrame(data=iris.data, columns=iris.feature_names)
@@ -37,28 +35,44 @@ print("\nTrain y Test:")
 print('\tX_train: ', X_train.shape, ' X_test: ', X_train.shape) 
 print('\ty_test: ', y_test.shape, ' y_train: ', y_train.shape)
 
+# inicializacion de pycaret
 train_data = X_train
 train_data['species'] = y_train
 s = setup(data=train_data, target='species', session_id=3)
 
+# se comparan todos los modelos disponibles en la libreria y se selecciona el mejor
 best = compare_models()
 
+# compara los hiperparametros de un modelo y selecciona el mejor, pero si es peor que el original devuelve el original
 tunned_model = tune_model(best)
 
+# realiza el metodo de ensanble de bagging
 print()
 print('Bagging')
 bagged_model = ensemble_model(tunned_model, method='Bagging')
 
+# realiza el metodo de ensanble de boosting
 print()
 print('Boosting')
 boosted_model = ensemble_model(tunned_model, method='Boosting')
 
+# selecciono el mejor de los 2 metodos de ensanble
 best = bagged_model
 
+# muestra una figura del pipeline del modelo
 evaluate_model(best)
 
+# entrena el modelo con los datos de test internos
 finalize_model(best)
 
-test_data = X_test
+# rendimiento del modelo con datos nuevos
+test_data = X_test.copy()
 test_data['species'] = y_test
 predict_model(best, data=test_data)
+
+# Matriz de confusión
+plt.rcdefaults()
+prediction = best.predict(X_test)
+ConfusionMatrixDisplay.from_predictions(y_test, prediction, display_labels=iris.target_names)
+plt.title(f"Matriz de confusión")
+plt.show()
